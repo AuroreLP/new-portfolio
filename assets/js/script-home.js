c5c0f63e1b67692d2794af529ac958a0218bd4b3
aurorelp <aleperff@gmail.com>
2025-10-31 14:24:59 +0100

    configured emailJS to send email

--- assets/js/script-home.js
+++ assets/js/script-home.js
@@ -116,3 +116,26 @@ navLinks.forEach(link => {
 
 window.addEventListener('scroll', updateSectionIndicator);
 updateSectionIndicator();
+
+// SEND EMAIL FROM CONTACT FORM
+const form = document.getElementById('contact-form');
+const messageEl = document.getElementById('form-message');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  emailjs.sendForm('service_l7c5sg4', 'template_bb4jbs8', this)
+    .then(() => {
+      messageEl.textContent = "✅ Message envoyé avec succès !";
+      messageEl.className = "success visible";
+      form.reset();
+      // Masquer après 5 secondes
+      setTimeout(() => messageEl.className = "hidden", 5000);
+    }, (error) => {
+      messageEl.textContent = "❌ Erreur lors de l’envoi. Veuillez réessayer.";
+      messageEl.className = "error visible";
+
+      // Masquer après 5 secondes
+      setTimeout(() => messageEl.className = "hidden", 5000);
+    });
+});
